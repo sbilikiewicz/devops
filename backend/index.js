f68f6836e50a9e83777554e3a1e35bbf9a23eba5
sbilikiewicz <>
2021-03-28 12:51:48 +0200

    App update

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,133 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+var uuid = require('uuid-random');
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-	host: "myredis",
-	port: 6379
-	//retry_strategy: () => 1000
-})
-
-redisClient.on('connect', () => {
-	console.log("Connected to redis server");
+    host: "myredis",
+    port: 6379
+    //retry_strategy: () => 1000
 });
 
-redisClient.exists("BoatType", function(err, reply) {
-	if (reply === 1) {
-		console.log("Key exists");
-	} else {
-		console.log("Key doesn't exist");
-		redisClient.set("BoatType", "submarine", function(err, reply) {
-			console.log(repl);
-		});
-	}
+redisClient.on('connect', () => {
+    console.log("Connected to redis server.")
 });
 
 const { Pool } = require('pg');
 
 const pgClient = new Pool({
-	user: "postgres", //zmienic
-	password: "witam",
-	database: "postgres",
-	host: "mypostgres",
-	port: "5432",
+    user: "postgres",
+    password: "123witam",
+    database: "postgres",
+    host: "mypostgres",
+    port: "5432"
 });
 
 pgClient.on('error', () => {
-	console.log("Postgres not connected");
+    console.log("Postgress not connected");
 });
 
-pgClient.query("CREATE TABLE IF NOT EXISTS VALUE (INT)")
-
-pgClient.connect(err => {
-    if (err) throw err;
-    else {
-        queryDatabase();
-    }
+pgClient.on('connect', () => {
+    console.log("Postgress connected");
 });
 
-function queryDatabase() {
-    const query = `
-        DROP TABLE IF EXISTS inventory;
-        CREATE TABLE inventory (id serial PRIMARY KEY, type VARCHAR(50), name VARCHAR(50));
-        INSERT INTO inventory (type, name) VALUES ('BoatType', 'submarine');
-    `;
+pgClient
+.query('CREATE TABLE IF NOT EXISTS boats (id VARCHAR(255), type VARCHAR(255), size VARCHAR(255), fullname VARCHAR(255))')
+.catch((err) => {
+    console.log(err);
+});
 
-    pgClient
-        .query(query)
-        .then(() => {
-            console.log('Table created successfully!');
-            pgClient.end(console.log('Closed client connection'));
-        })
-        .catch(err => console.log(err))
-        .then(() => {
-            console.log('Finished execution, exiting now');
-            process.exit();
-        });
-}
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+    res.send("Boats - welcome message")
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
-	console.log(`API Listening on port ${PORT}`);
+app.listen(PORT, ()=> {
+    console.log(`Api listetning on port ${PORT}`);
 });
+
+app.get('/boats/:type', (req, res) => {
+    try {
+      const boatType = req.params.type;
+    
+      // Redis
+      redisClient.get(boatType, async (err, type) => {
+        if (type) {
+          return res.status(200).send({
+            error: false,
+            message: `${boatType} detected`,
+            data: JSON.parse(type)
+          })
+        } else {
+			// No data in redis
+            console.log(`${boatType} Looking for boat type in postgres`)
+            pgClient.query('SELECT * FROM boats WHERE type = $1', [boatType], (error, results) => {
+                if (error) {
+                  throw error
+                }
+                redisClient.setex(boatType, 1440, JSON.stringify(results.rows));
+                res.status(200).json(results.rows)
+              })
+    
+        }
+      }) 
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.post('/boats', (request, response) => {
+    try {
+        console.log(request.body)
+        const {type, size, fullname} = request.body
+        const id = uuid();
+      
+        pgClient.query('INSERT INTO boats (id, type, size, fullname) VALUES ($1, $2, $3, $4)', [id, type, size, fullname], (error, results) => {
+          if (error) {
+            throw error
+          }
+          response.status(201).send(`Added boat ${id}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+const AddBoatEntry = (request, response) => {
+    const {type, size, fullname} = request.body
+    const id = uuid();
+  
+    pgClient.query('INSERT INTO boats (id, type, size, fullname) VALUES ($1, $2, $3, $4)', [id, type, size, fullname], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Added new boat entry`)
+    })
+}
+
+const getByName = (request, response) => {
+    const name = parseInt(request.params.fullname)
+  
+    pgClient.query('SELECT * FROM boats', (error, results) => {
+      if (error) {
+        throw error
+      }
+      return results.rows
+    })
+}
+
+
+
